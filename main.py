9b433e97afe79eb1fb9a1fe94e64bd9ada38d60f
Kinga Olszewska <150083424+Kinia7@users.noreply.github.com>
2024-05-24 23:39:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import asyncio
 import aiohttp
 from datetime import datetime, timedelta
 
-API_URL = "https://api.nbp.pl/api/exchangerates/tables/a/"
+API_URL = "https://api.nbp.pl/api/exchangerates/tables/A"
 TODAY = datetime.today().strftime("%Y-%m-%d")
 
-async def get_rates(currency, date):
-    """Pobiera kursy wymiany dla danego dnia i waluty."""
-    url = f"{API_URL}{currency}/{date}?format=json"
+async def get_rates(date):
+    """Pobiera wszystkie kursy wymiany dla danego dnia."""
+    url = f"{API_URL}/{date}/?format=json"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             if response.status == 200:
@@ -19,19 +19,25 @@ async def get_rates(currency, date):
                 raise RuntimeError(f"Błąd pobierania danych: {response.status}")
 
 async def main(days):
-    """Pobiera kursy wymiany dla kilku dni i walut."""
+    """Pobiera kursy wymiany dla kilku dni."""
     currencies = ['EUR', 'USD']
-    for currency in currencies:
-        print(f"Kursy wymiany dla waluty: {currency}")
-        tasks = [get_rates(currency, (datetime.strptime(TODAY, "%Y-%m-%d") - timedelta(days=i)).strftime("%Y-%m-%d")) for i in range(days)]
-        results = await asyncio.gather(*tasks)
-        for i, result in enumerate(results, 1):
-            print(f"Dane z dnia {datetime.strptime(TODAY, '%Y-%m-%d') - timedelta(days=i)}:")
-            if result is not None:  
-                for currency_data in result[0]["rates"]:
-                    if currency_data['code'] == currency:
-                        print(f"{currency_data['code']:3} {currency_data['mid']:.4f}")
-            print()
+    tasks = [get_rates((datetime.strptime(TODAY, "%Y-%m-%d") - timedelta(days=i)).strftime("%Y-%m-%d")) for i in range(days)]
+    results = await asyncio.gather(*tasks)
+    
+    for i, result in enumerate(results):
+        date = (datetime.strptime(TODAY, "%Y-%m-%d") - timedelta(days=i)).strftime("%Y-%m-%d")
+        print(f"Dane z dnia {date}:")
+        if result:
+            rates = result[0]["rates"]
+            for currency in currencies:
+                rate_info = next((rate for rate in rates if rate['code'] == currency), None)
+                if rate_info:
+                    print(f"{rate_info['code']}: {rate_info['mid']:.4f}")
+                else:
+                    print(f"Brak danych dla {currency}")
+        else:
+            print("Brak danych")
+        print()
 
 if __name__ == "__main__":
     try:
